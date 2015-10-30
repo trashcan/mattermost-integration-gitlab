c191533e8561cdd353b2e75985e73ab092722e73
Pat Lathem <patl@codero.com>
2015-10-30 14:36:32 -0500

    Simple startup script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export MATTERMOST_WEBHOOK_URL=https://chat.codero.com/hooks/ewzp5ih557gobpq8pkr1asd8jc
+export MATTERMOST_WEBHOOK_URL=
 export USERNAME=rssbot
 export ICON_URL=https://news.ycombinator.com/y18.gif
 export RSS_URL=https://news.ycombinator.com/rss
